30a5db792ac20ea2f1021817a24ff871438292d9
Lightning Lion <zj56789012@outlook.com>
2025-01-14 13:40:55 +0800

    Update SwiftUILifecycleToolKit.swift

--- Sources/SwiftUILifecycleToolKit/SwiftUILifecycleToolKit.swift
+++ Sources/SwiftUILifecycleToolKit/SwiftUILifecycleToolKit.swift
@@ -11,9 +11,9 @@ import SwiftUI
 @available(iOS 17.0, watchOS 10.0, macOS 14.0, *)
 extension View {
     @ViewBuilder
-    public func onLoad(_ action:@escaping () -> ()) -> some View {
+    public func onLoad(perform:@escaping () -> ()) -> some View {
         self
-            .modifier(OnLoad(action: action))
+            .modifier(OnLoad(action: perform))
     }
 }
 
@@ -42,6 +42,7 @@ extension View {
     public func onChange<V>(of value: V, onLoad: Bool = false, _ action: @escaping (_ oldValue: V, _ newValue: V) -> Void) -> some View where V : Equatable {
         if onLoad {
             self
+                .modifier(OnChange(value: value, action: action))
         } else {
             self
                 .onChange(of: value, initial: false, action)
@@ -62,3 +63,132 @@ struct OnChange<V>: ViewModifier where V : Equatable {
             .onChange(of: value, initial: false, action)
     }
 }
+
+// MARK: 提供.task(onLoad:true)扩展，避免难以调试的Bug（意外触发了多次加载）
+@available(iOS 17.0, watchOS 10.0, macOS 14.0, *)
+extension View {
+    @ViewBuilder
+    @inlinable public func task(priority: TaskPriority = .userInitiated, onLoad: Bool, _ action: @escaping @Sendable () async -> Void) -> some View {
+        if onLoad {
+            self
+                .modifier(TaskOnLoad(priority: priority, action: action))
+        } else {
+            self
+                .task(priority: priority, action)
+        }
+    }
+}
+
+public
+struct TaskOnLoad: ViewModifier {
+    var priority: TaskPriority = .userInitiated
+    var action: @Sendable () async -> Void
+    public init(priority: TaskPriority, action: @Sendable @escaping () async -> Void) {
+        self.priority = priority
+        self.action = action
+    }
+    @State
+    private var task:Task<Void,Never>? = nil
+    public func body(content: Content) -> some View {
+        content
+            .onLoad {
+                self.task = Task(priority: priority, operation: action)
+            }
+            .onDestroy {
+                Task { @MainActor in
+                    await MainActor.run {
+                        self.task?.cancel()
+                    }
+                }
+            }
+    }
+}
+
+// MARK: NavigationStack的设计是，导航堆栈中的页面会保持自己的状态，而直接在页面上使用.onDisappear来做释放工作，会导致过早触发.onDisappear（在下一个页面Push出来的时候就触发了onDisappear）。使用.onDestroy，会在本页面Pop掉时才触发，完成清理工作。
+@MainActor
+fileprivate
+class LifeDetector {
+    private
+    var onDestroy: @Sendable ()->()
+    init(perform: @Sendable @escaping ()->()) {
+        self.onDestroy = perform
+    }
+    deinit {
+        self.onDestroy()
+    }
+}
+
+fileprivate
+struct OnDestroyPack: ViewModifier {
+    // 我持有这个对象，这样在我销毁的时候，这个对象也会销毁
+    @State
+    var mod:LifeDetector
+    func body(content: Content) -> some View {
+        content
+    }
+}
+
+extension View {
+    @ViewBuilder
+    public func onDestroy(perform: @escaping ()->()) -> some View {
+        self
+            .modifier(OnDestroyPack(mod:.init(perform: {
+                // class的deinit会在任意线程被调用，让我们总是回到主线程，来处理SwiftUI托付给我们的perform闭包。
+                Task { @MainActor in
+                    await MainActor.run {
+                        perform()
+                    }
+                }
+            })))
+    }
+}
+
+// 使用方法
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @State
+//    private var presentPage1 = false
+//    var body: some View {
+//        NavigationStack {
+//            Button("Go Page1", action: {
+//                presentPage1 = true
+//            })
+//            .navigationDestination(isPresented: $presentPage1, destination: {
+//                Page1()
+//            })
+//        }
+//    }
+//}
+//
+//
+//struct Page1: View {
+//    @State
+//    private var presentPage2 = false
+//
+//    var body: some View {
+//
+//        VStack(content: {
+//            Button("Go Page2", action: {
+//                presentPage2 = true
+//            })
+//        })
+//        .onDestroy {
+//            print("Page1页面销毁")
+//        }
+//        .navigationDestination(isPresented: $presentPage2, destination: {
+//            Page2()
+//        })
+//    }
+//}
+//
+//struct Page2: View {
+//    var body: some View {
+//        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+//    }
+//}
+//
+//#Preview {
+//    ContentView()
+//}
+//
